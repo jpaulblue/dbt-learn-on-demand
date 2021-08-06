22dedd5e1ebd7d8e9b368d364a61438334789b67
jablue@geico.com <jablue@geico.com>
2021-08-06 20:21:22 +0000

    update2

--- models/staging/jaffle_Shop/stg_customers.sql
+++ models/staging/jaffle_Shop/stg_customers.sql
@@ -1,12 +1,11 @@
 with customers as (
-
-    select
+    
+    select 
         id as customer_id,
         first_name,
         last_name
 
     from raw.jaffle_shop.customers
-
 )
 
 select * from customers
